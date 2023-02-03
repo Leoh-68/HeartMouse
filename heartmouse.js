e2b7ecc0479e5f3b13dbb68e970147f74ad47ecf
Nguyễn Đức Huy <84003662+NguyenDucHuy-CDTH19PMC@users.noreply.github.com>
2023-02-03 10:13:08 +0700

    Update heartmouse.js

--- heartmouse.js
+++ heartmouse.js
@@ -1,4 +1,4 @@
-function Heartmouse(c = ".heart",i = "12",dpi = "30",d = "700",r = "40") {
+function Heartmouse(c = "heart",i = "12",dpi = "30",d = "700",r = "40") {
   var _default = {
     class:c,
     items:i,
@@ -7,19 +7,19 @@ function Heartmouse(c = ".heart",i = "12",dpi = "30",d = "700",r = "40") {
     radius:r
   };
   $(document).ready(function () {
-		$('footer').append("<div class='"+_default.class+"'></>");
-		if(isExist(_default.class)){
-			$(_default.class).append('<div class="heart-icon"></div>')
+		$('body').append("<div class='"+_default.class+"'></>");
+		if(isExist('.'+_default.class)){
+			$('.'+_default.class).append('<div class="heart-icon"></div>')
 		}
 		$(document).mousemove(function (e) {
 				var mousex = e.clientX;
 				var mousey = e.clientY;
-				$(_default.class).css({"top":mousey,"left":mousex});
+				$('.'+_default.class).css({"top":mousey,"left":mousex});
 		})
 		$(document).click(function (e) { 
 			$('div.partical-box').remove();
-			if(isExist(_default.class)){
-				$(_default.class).append('<div class="partical-box" style="transform:translate(-50%,-50%);width:15px;height:15px"></div>');
+			if(isExist('.'+_default.class)){
+				$('.'+_default.class).append('<div class="partical-box" style="transform:translate(-50%,-50%);width:15px;height:15px"></div>');
 				for (let index = 0; index < _default.items; index++) {
 				$('div.partical-box').append('<div class="partical"></div>');
 				$('div.partical').eq(index).css({transform:'rotate('+(index)*_default.degperitem+'deg)'});
